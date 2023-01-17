e6082f0e9165cdddc0b82de48f5f712c92a0d922
krzgr <kgrund2205@gmail.com>
2023-01-17 11:44:23 +0100

    Deleted cmake-build-debug folder

--- cmake-build-debug/googletest-master/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Pc/Documents/AGH/3semestr/zaawansowane_programowanie_obiektowe/Projekt_NetSim/googletest-master/googlemock
-# Build directory: C:/Users/Pc/Documents/AGH/3semestr/zaawansowane_programowanie_obiektowe/Projekt_NetSim/cmake-build-debug/googletest-master/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- cmake-build-debug/googletest-master/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Pc/Documents/AGH/3semestr/zaawansowane_programowanie_obiektowe/Projekt_NetSim/googletest-master/googletest
-# Build directory: C:/Users/Pc/Documents/AGH/3semestr/zaawansowane_programowanie_obiektowe/Projekt_NetSim/cmake-build-debug/googletest-master/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
